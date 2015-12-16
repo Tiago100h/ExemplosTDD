a0bd7174d70acb25b45ba67225e8be7e8b72e2e6
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2015-12-16 01:18:04 -0200

    Testes do capítulo 2

---
+++ ExemplosTDD/ExemplosTDD.VB.Test/Capitulo02/TestaMaiorEMenor.vb
@@ -0,0 +1,30 @@
+﻿Imports ExemplosTDD.VB.Capitulo02
+
+<TestClass()> Public Class TestaMaiorEMenor
+
+    <TestMethod()> Public Sub OrdemDecresecente()
+        Dim carrinho = New CarrinhoDeCompras()
+        carrinho.Adiciona(New Produto("Geladeira", 450.0))
+        carrinho.Adiciona(New Produto("Liquidificador", 250.0))
+        carrinho.Adiciona(New Produto("Jogo de pratos", 70.0))
+
+        Dim algoritimo = New MaiorEMenor()
+        algoritimo.Encontra(carrinho)
+
+        Assert.AreEqual("Jogo de pratos", algoritimo.Menor.Nome)
+        Assert.AreEqual("Geladeira", algoritimo.Maior.Nome)
+    End Sub
+
+    <TestMethod> Public Sub ApenasUmProduto()
+        Dim carrinho = New CarrinhoDeCompras()
+        Dim geladeira = New Produto("Geladeira", 450.0)
+        carrinho.Adiciona(geladeira)
+
+        Dim algoritimo = New MaiorEMenor()
+        algoritimo.Encontra(carrinho)
+
+        Assert.AreEqual(geladeira, algoritimo.Menor)
+        Assert.AreEqual(geladeira, algoritimo.Maior)
+    End Sub
+
+End Class
--- ExemplosTDD/ExemplosTDD.VB/Capitulo02/MaiorEMenor.vb
+++ ExemplosTDD/ExemplosTDD.VB/Capitulo02/MaiorEMenor.vb
@@ -5,9 +5,10 @@
 
         Public Sub Encontra(carrinho As CarrinhoDeCompras)
             For Each produto As Produto In carrinho.Produtos
-                If Menor Is Nothing Or produto.Valor < Menor.Valor
+                If Menor Is Nothing OrElse produto.Valor < Menor.Valor
                     Menor = produto
-                ElseIf Maior Is Nothing Or produto.Valor > Maior.Valor
+                End If
+                If Maior Is Nothing OrElse produto.Valor > Maior.Valor
                     Maior = produto
                 End If
             Next
