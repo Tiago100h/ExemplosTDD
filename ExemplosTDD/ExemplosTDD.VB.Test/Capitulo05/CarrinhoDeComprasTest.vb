2caabc132bb3d42f65f9364c03db6af5a50e5ee9
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2015-12-21 09:42:00 -0200

    Correção do capítulo 5

--- ExemplosTDD/ExemplosTDD.VB.Test/Capitulo05/CarrinhoDeComprasTest.vb
+++ ExemplosTDD/ExemplosTDD.VB.Test/Capitulo05/CarrinhoDeComprasTest.vb
@@ -1,12 +1,13 @@
 ﻿Imports ExemplosTDD.VB.Capitulo05
 Namespace Capitulo05
+
     <TestClass()>
-    Public Class MaiorPrecoTest
+    Public Class CarrinhoDeComprasTest
 
         <TestMethod()>
         Public Sub DeveRetornarZeroSeCarrinhoVazio()
             Dim carrinho As New CarrinhoDeCompras
-            
+
             Assert.AreEqual(0, carrinho.MaiorValor(), 0.0001)
         End Sub
 
@@ -14,7 +15,7 @@ Namespace Capitulo05
         Public Sub DeveRetornarValorDoItemSeCarrinhoCom1Elemento()
             Dim carrinho As New CarrinhoDeCompras
             carrinho.Adiciona(New Item("Geladeira", 1, 900))
-            
+
             Assert.AreEqual(900, carrinho.MaiorValor(), 0.0001)
         End Sub
 
@@ -24,7 +25,7 @@ Namespace Capitulo05
             carrinho.Adiciona(New Item("Geladeira", 1, 900))
             carrinho.Adiciona(New Item("Fogão", 1, 1500))
             carrinho.Adiciona(New Item("Máquina de lavar", 1, 750))
-            
+
             Assert.AreEqual(1500, carrinho.MaiorValor(), 0.0001)
         End Sub
 
