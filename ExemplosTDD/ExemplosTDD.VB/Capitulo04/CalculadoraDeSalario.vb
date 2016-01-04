d1fa018cf9265670da03c6e10a22e9b8127d0637
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2016-01-04 14:46:15 -0200

    Voltando implementação do livro

--- ExemplosTDD/ExemplosTDD.VB/Capitulo04/CalculadoraDeSalario.vb
+++ ExemplosTDD/ExemplosTDD.VB/Capitulo04/CalculadoraDeSalario.vb
@@ -2,16 +2,10 @@
     Public Class CalculadoraDeSalario
         Public Function CalculaSalario(ByVal funcionario As Funcionario) As Double
 
-            If funcionario.Cargo = Cargo.Desenvolvedor Then
-                If funcionario.Salario > 3000 Then
-                    Return funcionario.Salario * 0.8
-                End If
-                Return funcionario.Salario * 0.9
-            End If
             If funcionario.Salario > 3000 Then
-                Return funcionario.Salario * 0.75
+                Return funcionario.Salario * 0.8
             End If
-            Return funcionario.Salario * 0.85
+            Return funcionario.Salario * 0.9
 
         End Function
     End Class
